9952164c8a518d6dac32dc27af0ce7516198eec1
Joshua Gralewski <jgralews3@yahoo.com>
2020-08-16 19:12:33 -0500

    finished MVP

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,26 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+
+const setup = (field) => {
+    const utils = render(<CheckoutForm />)
+    const input = utils.getByTestId(field)
+    return {
+      input,
+      ...utils,
+    }
+  }
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    const {getByText} = render(<CheckoutForm />)
+    getByText(/checkout form/i)
 
-test("form shows success message on submit with form details", () => {});
+});
+
+test("form shows success message on submit with form details", () => {
+    const {getByText} = render(<CheckoutForm />)
+    fireEvent.click(getByText('Checkout'))
+    getByText(/You have ordered some plants/i)
+});
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,13 @@
-// write your custom hook here to control your checkout form
+import React from 'react';
+
+const useForm = (initialValue) => {
+    const [values, setValues] = React.useState(initialValue)
+    console.log(values);
+    const handleChanges = e => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+    }
+
+    return [values, handleChanges]
+}
+
+export default useForm;
